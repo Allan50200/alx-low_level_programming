35bcb00659a37a8d662ef5bf30bd88e3b7c4f022
Allan50200 <allannyagi13@gmail.com>
2022-06-30 09:12:21 -0700

    changed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
 /**
-*main - the entry point of the program
+* main - entry point
 *
-*This program outputs a string
-* *returns void
-*
+* uses puts function instead
+* return - returns 0
 */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
