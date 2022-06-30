afa0583ca3b6cff1d113994c4a537cae971c0199
Allan50200 <allannyagi13@gmail.com>
2022-06-30 09:30:14 -0700

    printf c program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * The function will output a string
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n","with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}	
