8e4bc05aa82b05494c1c5e268ef6bf1b36796463
Allan50200 <allannyagi13@gmail.com>
2022-06-30 13:19:43 -0700

    Hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,15 +6,11 @@
  * Description : using the main function
  * this program prints "programming is like building a multilingual puzzle
  * Return : 0
- *
- * Description: returns success
  */
-
 int main(void)
 {
 
 		puts("\"Programming is like building a multilingual puzzle");
 		return (0);
+
 }
-
-
