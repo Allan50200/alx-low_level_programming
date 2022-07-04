82dd5d693c2688027f431900d1ce94e55d054bf0
Allan50200 <allannyagi13@gmail.com>
2022-07-04 00:30:05 -0700

    Prints numbers from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the entry poin
+ *
+ * Description: this program prints numbers starting 10
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d \n", num);
+	}
+
+	return (0);
+}
